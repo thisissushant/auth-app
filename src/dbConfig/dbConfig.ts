d6ff13375969cf9fce99dc2c2b29c9b6b654b234
Sushant Singh <sushantbibhu@gmail.com>
2024-02-08 17:17:27 +0530

    config database

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+export async function Connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB Connected Successfuly");
+    });
+    connection.on("error", (err) => {
+      console.log(
+        "Mongo connection error. Please make sure MongoDB is running" + err
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log("somthing went wrong!");
+    console.log(error);
+  }
+}
