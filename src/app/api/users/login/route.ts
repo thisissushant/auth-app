37c60aced129f2e6148a0fc1471be3ecce43f8c2
Sushant Singh <sushantbibhu@gmail.com>
2024-02-10 23:15:56 +0530

    Implement login functionality with JWT authentication

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,41 @@
+import { Connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+Connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 400 });
+    }
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid Password" }, { status: 400 });
+    }
+    const tokenData = {
+      id: user._id,
+      email: user.email,
+      username: user.username,
+    };
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login Successfully",
+      success: "true",
+    });
+    response.cookies.set("token", token, { httpOnly: true });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,39 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 export default function Login() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      toast.success("Logged in successfully!");
+      router.push("/profile");
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-red-300">
-      <h1 className="text-black-900">Login</h1>
+      <h1 className="text-black-900">{loading ? "Please wait..." : "Login"}</h1>
       <hr />
       <label htmlFor="name" className="sr-only ">
         email
